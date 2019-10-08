19286307c2e33256ab9e7d5c7b4fd9bdef638abd
Daniel <jodagito@gmail.com>
2019-10-07 19:22:22 -0500

    BillValue
    Retrieves an user bill value

---
+++ SegundaEntrega/BillValue.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace SegundaEntrega
+{
+    partial class BillValue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIdCard = new System.Windows.Forms.TextBox();
+            this.btnQuery = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblBillValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cédula del Usuario";
+            // 
+            // txtIdCard
+            // 
+            this.txtIdCard.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtIdCard.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIdCard.Location = new System.Drawing.Point(19, 69);
+            this.txtIdCard.Name = "txtIdCard";
+            this.txtIdCard.Size = new System.Drawing.Size(238, 38);
+            this.txtIdCard.TabIndex = 1;
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnQuery.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuery.Location = new System.Drawing.Point(68, 191);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(148, 58);
+            this.btnQuery.TabIndex = 2;
+            this.btnQuery.Text = "Consultar";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Valor";
+            // 
+            // lblBillValue
+            // 
+            this.lblBillValue.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblBillValue.AutoSize = true;
+            this.lblBillValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBillValue.Location = new System.Drawing.Point(96, 135);
+            this.lblBillValue.Name = "lblBillValue";
+            this.lblBillValue.Size = new System.Drawing.Size(0, 31);
+            this.lblBillValue.TabIndex = 4;
+            // 
+            // BillValue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblBillValue);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnQuery);
+            this.Controls.Add(this.txtIdCard);
+            this.Controls.Add(this.label1);
+            this.Name = "BillValue";
+            this.Text = "Valor Factura";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIdCard;
+        private System.Windows.Forms.Button btnQuery;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblBillValue;
+    }
+}
---
+++ SegundaEntrega/BillValue.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SegundaEntrega
+{
+    public partial class BillValue : Form
+    {
+        List<User> stored_users;
+        User user;
+        public BillValue(List<User> stored_users)
+        {
+            this.stored_users = stored_users;
+            InitializeComponent();
+        }
+
+        public void getUserBillValue(string id_card)
+        {
+            foreach (User user in stored_users)
+            {
+                if (user.idCard == id_card)
+                {
+                    this.user = user;
+                    lblBillValue.Text = user.calculateBill().ToString();
+                    break;
+                }
+                else if (user == stored_users.Last())
+                {
+                    MessageBox.Show("No existe un usuario con esa cédula.");
+                    txtIdCard.Text = "";
+                    lblBillValue.Text = "";
+                }
+            }
+            if (stored_users.Count() == 0)
+            {
+                MessageBox.Show("No existen usuarios registrados.");
+                txtIdCard.Text = "";
+            }
+        }
+
+        private void btnQuery_Click(object sender, EventArgs e)
+        {
+            string id_card = txtIdCard.Text;
+            getUserBillValue(id_card);
+        }
+    }
+}
