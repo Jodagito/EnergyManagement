0dab68eb18b3d0e5de4667b2794774749239a29b
Daniel <jodagito@gmail.com>
2019-10-07 19:23:56 -0500

    DiscountsTotal
    Returns the discounts total.

---
+++ SegundaEntrega/DiscountsTotal.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace SegundaEntrega
+{
+    partial class DiscountsTotal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblDiscount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-1, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(298, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor Total Descontado";
+            // 
+            // lblDiscount
+            // 
+            this.lblDiscount.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblDiscount.AutoSize = true;
+            this.lblDiscount.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiscount.Location = new System.Drawing.Point(150, 92);
+            this.lblDiscount.Name = "lblDiscount";
+            this.lblDiscount.Size = new System.Drawing.Size(0, 31);
+            this.lblDiscount.TabIndex = 1;
+            // 
+            // DiscountsTotal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 145);
+            this.Controls.Add(this.lblDiscount);
+            this.Controls.Add(this.label1);
+            this.Name = "DiscountsTotal";
+            this.Text = "Descuento Total";
+            this.Load += new System.EventHandler(this.DiscountsTotal_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDiscount;
+    }
+}
---
+++ SegundaEntrega/DiscountsTotal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SegundaEntrega
+{
+    public partial class DiscountsTotal : Form
+    {
+        Users users_stack;
+        public DiscountsTotal(Users users_stack)
+        {
+            this.users_stack = users_stack;
+            InitializeComponent();
+        }
+
+        private void DiscountsTotal_Load(object sender, EventArgs e)
+        {
+            float incentives_total = users_stack.calculateTotalIncentives();
+            string incentives_total_str = users_stack.calculateTotalIncentives().ToString();
+            if (float.IsNaN(incentives_total))
+            {
+                incentives_total_str = "0";
+            }
+            lblDiscount.Text = incentives_total_str;
+        }
+    }
+}
