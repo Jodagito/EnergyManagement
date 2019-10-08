fc8adced94c17fb5d53dcdcdc70d206b03d859d7
Daniel <jodagito@gmail.com>
2019-10-07 19:23:40 -0500

    ConsumeAverage
    Returns the consume average.

---
+++ SegundaEntrega/ConsumeAverage.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace SegundaEntrega
+{
+    partial class ConsumeAverage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(427, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "El promedio de consumo actual es";
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverage.Location = new System.Drawing.Point(12, 63);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(0, 31);
+            this.lblAverage.TabIndex = 1;
+            // 
+            // ConsumeAverage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 131);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.label1);
+            this.Name = "ConsumeAverage";
+            this.Text = "Promedio de Consumo";
+            this.Load += new System.EventHandler(this.ConsumeAverage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblAverage;
+    }
+}
---
+++ SegundaEntrega/ConsumeAverage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SegundaEntrega
+{
+    public partial class ConsumeAverage : Form
+    {
+        Users users_stack;
+        public ConsumeAverage(Users users_stack)
+        {
+            this.users_stack = users_stack;
+            InitializeComponent();
+        }
+
+        private void ConsumeAverage_Load(object sender, EventArgs e)
+        {
+            float average = users_stack.calculateConsumeAverage();
+            string average_str = users_stack.calculateConsumeAverage().ToString() + "Kw";
+            if (float.IsNaN(average))
+            {
+                average_str = "No hay datos registrados.";
+            }
+            lblAverage.Text = average_str;
+        }
+    }
+}
