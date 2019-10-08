41c5dcf31cda067d653d1219a6db48a57c8a077b
Daniel <jodagito@gmail.com>
2019-10-07 19:24:37 -0500

    Register form

--- SegundaEntrega/Form2.Designer.cs
+++ SegundaEntrega/Form2.Designer.cs
@@ -28,20 +28,177 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.consumeTxt = new System.Windows.Forms.TextBox();
+            this.goalTxt = new System.Windows.Forms.TextBox();
+            this.stratumTxt = new System.Windows.Forms.TextBox();
+            this.consumeLbl = new System.Windows.Forms.Label();
+            this.goalLbl = new System.Windows.Forms.Label();
+            this.stratumLabel = new System.Windows.Forms.Label();
+            this.idCardLbl = new System.Windows.Forms.Label();
+            this.idCardTxt = new System.Windows.Forms.TextBox();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.registerBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // consumeTxt
+            // 
+            this.consumeTxt.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.consumeTxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.consumeTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.consumeTxt.Location = new System.Drawing.Point(297, 283);
+            this.consumeTxt.Name = "consumeTxt";
+            this.consumeTxt.Size = new System.Drawing.Size(142, 23);
+            this.consumeTxt.TabIndex = 22;
+            // 
+            // goalTxt
+            // 
+            this.goalTxt.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.goalTxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.goalTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.goalTxt.Location = new System.Drawing.Point(297, 230);
+            this.goalTxt.Name = "goalTxt";
+            this.goalTxt.Size = new System.Drawing.Size(142, 23);
+            this.goalTxt.TabIndex = 21;
+            // 
+            // stratumTxt
+            // 
+            this.stratumTxt.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.stratumTxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.stratumTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stratumTxt.Location = new System.Drawing.Point(297, 176);
+            this.stratumTxt.Name = "stratumTxt";
+            this.stratumTxt.Size = new System.Drawing.Size(142, 23);
+            this.stratumTxt.TabIndex = 20;
+            // 
+            // consumeLbl
+            // 
+            this.consumeLbl.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.consumeLbl.AutoSize = true;
+            this.consumeLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.consumeLbl.Location = new System.Drawing.Point(22, 272);
+            this.consumeLbl.Name = "consumeLbl";
+            this.consumeLbl.Size = new System.Drawing.Size(210, 31);
+            this.consumeLbl.TabIndex = 19;
+            this.consumeLbl.Text = "Consumo actual";
+            // 
+            // goalLbl
+            // 
+            this.goalLbl.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.goalLbl.AutoSize = true;
+            this.goalLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.goalLbl.Location = new System.Drawing.Point(22, 219);
+            this.goalLbl.Name = "goalLbl";
+            this.goalLbl.Size = new System.Drawing.Size(228, 31);
+            this.goalLbl.TabIndex = 18;
+            this.goalLbl.Text = "Meta de consumo";
+            // 
+            // stratumLabel
+            // 
+            this.stratumLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.stratumLabel.AutoSize = true;
+            this.stratumLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stratumLabel.Location = new System.Drawing.Point(22, 165);
+            this.stratumLabel.Name = "stratumLabel";
+            this.stratumLabel.Size = new System.Drawing.Size(101, 31);
+            this.stratumLabel.TabIndex = 17;
+            this.stratumLabel.Text = "Estrato";
+            // 
+            // idCardLbl
+            // 
+            this.idCardLbl.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.idCardLbl.AutoSize = true;
+            this.idCardLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idCardLbl.Location = new System.Drawing.Point(22, 115);
+            this.idCardLbl.Name = "idCardLbl";
+            this.idCardLbl.Size = new System.Drawing.Size(100, 31);
+            this.idCardLbl.TabIndex = 16;
+            this.idCardLbl.Text = "Cédula";
+            // 
+            // idCardTxt
+            // 
+            this.idCardTxt.AccessibleDescription = "Cédula del usuario";
+            this.idCardTxt.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.idCardTxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.idCardTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idCardTxt.Location = new System.Drawing.Point(297, 126);
+            this.idCardTxt.Name = "idCardTxt";
+            this.idCardTxt.Size = new System.Drawing.Size(142, 23);
+            this.idCardTxt.TabIndex = 15;
+            this.idCardTxt.Tag = "";
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.clearBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearBtn.Location = new System.Drawing.Point(297, 359);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(142, 47);
+            this.clearBtn.TabIndex = 14;
+            this.clearBtn.Text = "Limpiar";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // registerBtn
+            // 
+            this.registerBtn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.registerBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.registerBtn.Location = new System.Drawing.Point(25, 359);
+            this.registerBtn.Name = "registerBtn";
+            this.registerBtn.Size = new System.Drawing.Size(142, 47);
+            this.registerBtn.TabIndex = 13;
+            this.registerBtn.Text = "Registrar";
+            this.registerBtn.UseVisualStyleBackColor = true;
+            this.registerBtn.Click += new System.EventHandler(this.registerBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(81, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(335, 91);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Registro";
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(703, 584);
+            this.ClientSize = new System.Drawing.Size(476, 448);
+            this.Controls.Add(this.consumeTxt);
+            this.Controls.Add(this.goalTxt);
+            this.Controls.Add(this.stratumTxt);
+            this.Controls.Add(this.consumeLbl);
+            this.Controls.Add(this.goalLbl);
+            this.Controls.Add(this.stratumLabel);
+            this.Controls.Add(this.idCardLbl);
+            this.Controls.Add(this.idCardTxt);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.registerBtn);
+            this.Controls.Add(this.label1);
             this.Name = "Form2";
-            this.Text = "Form2";
+            this.Text = "Registro";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox consumeTxt;
+        private System.Windows.Forms.TextBox goalTxt;
+        private System.Windows.Forms.TextBox stratumTxt;
+        private System.Windows.Forms.Label consumeLbl;
+        private System.Windows.Forms.Label goalLbl;
+        private System.Windows.Forms.Label stratumLabel;
+        private System.Windows.Forms.Label idCardLbl;
+        private System.Windows.Forms.TextBox idCardTxt;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button registerBtn;
+        private System.Windows.Forms.Label label1;
     }
 }
--- SegundaEntrega/Form2.cs
+++ SegundaEntrega/Form2.cs
@@ -12,16 +12,75 @@ namespace SegundaEntrega
 {
     public partial class Form2 : Form
     {
+        List<User> stored_users;
         Users users_stack;
-        public Form2(Users users_stack)
+        public Form2(List<User> stored_users, Users users_stack)
         {
+            this.stored_users = stored_users;
             this.users_stack = users_stack;
             InitializeComponent();
         }
 
         private void Form2_Load(object sender, EventArgs e)
         {
+            MessageBox.Show("Bienvenido, para continuar por favor registre un usuario");
+        }
 
+        private void registerBtn_Click(object sender, EventArgs e)
+        {
+            bool save = true;
+            if (idCardTxt.Text == "" || stratumTxt.Text == "" || goalTxt.Text == "" || consumeTxt.Text == "")
+            {
+                MessageBox.Show("Todos los campos son obligatorios");
+                save = false;
+            }
+            if (save)
+            {
+                foreach (User user in stored_users)
+                {
+                    if (user.idCard == idCardTxt.Text)
+                    {
+                        MessageBox.Show("Un usuario con esta cédula ya está registrado");
+                        save = false;
+                    }
+                }
+                string id_card = idCardTxt.Text;
+                if (float.Parse(stratumTxt.Text) > 6 || float.Parse(stratumTxt.Text) < 1)
+                {
+                    MessageBox.Show("El estrato debe estar entre uno y seis");
+                    save = false;
+                }
+                else if (float.Parse(goalTxt.Text) < 1)
+                {
+                    MessageBox.Show("La meta de consumo no puede ser negativa o igual a cero");
+                    save = false;
+                }
+                else if (float.Parse(consumeTxt.Text) < 1)
+                {
+                    MessageBox.Show("El consumo actual no puede ser negativo o igual a cero");
+                    save = false;
+                }
+                else if (save)
+                {
+                    string stratum = stratumTxt.Text;
+                    float save_goal = float.Parse(goalTxt.Text);
+                    float actual_consume = float.Parse(consumeTxt.Text);
+                    User actual_user = new User(id_card, stratum, save_goal, actual_consume);
+                    stored_users.Add(actual_user);
+                    users_stack.registeredUsers = stored_users;
+                    MessageBox.Show("Usuario registrado con éxito");
+                    clearBtn_Click(sender, e);
+                    this.Close();
+                }
+            }
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            idCardTxt.Text = "";
+            stratumTxt.Text = "";
+            goalTxt.Text = "";
+            consumeTxt.Text = "";
         }
     }
 }
